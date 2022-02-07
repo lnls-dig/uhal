9ca0eeb34e626d66795b4a5f41834567201ded05
Érico Nogueira <erico.rolim@lnls.br>
2022-02-07 09:44:34 -0300

    Use PciDevice class.
    This required changing the command line interface to receive the device
    number instead of the whole path to it.
    
    This commit introduces a bug where the mmap'd regions aren't unmapped
    before exiting. We should try to fix this in the PciDevice class
    instead.

--- decode-reg.cc
+++ decode-reg.cc
@@ -12,7 +12,6 @@
 #include <cstdlib>
 #include <cstring>
 
-#include <pciDriver/lib/pciDriver.h>
 #include <pciDriver/lib/PciDevice.h>
 
 extern "C" {
@@ -25,27 +24,19 @@ static void print_help (char *program_name)
     fprintf (stdout, "Simple FPGA Register Reader\n"
             "Usage: %s [options]\n"
             "  -h                        Display this usage information\n"
-            "  -b <Device File>          Device file\n"
+            "  -b <Device Number>        Device number as used in /dev/fpga-N\n"
             "  -v                        Verbose output\n"
             "  -a <FPGA Address in hex>  Base address to start reads from\n",
             program_name);
 }
 
-static void unmap_bar(pd_device_t *pci_handle, unsigned bar, void *ptr)
-{
-    if (ptr)
-        pd_unmapBAR(pci_handle, bar, ptr);
-}
-
 int main(int argc, char *argv[])
 {
     int verbose = 0;
-    char *devicefile_str = NULL;
+    char *device_number_str = NULL;
     char *address_str = NULL;
     int opt;
-    pd_device_t m_dev = {0};
-    pd_device_t *dev = &m_dev;
-	
+
     while ((opt = getopt (argc, argv, "hb:va:")) != -1) {
         /* Get the user selected options */
         switch (opt) {
@@ -54,7 +45,7 @@ int main(int argc, char *argv[])
                 print_help (argv [0]);
                 return 1;
             case 'b':
-                devicefile_str = optarg;
+                device_number_str = optarg;
                 break;
             case 'v':
                 verbose = 1;
@@ -69,13 +60,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    /* Device file is mandatory */
-    if (devicefile_str == NULL) {
-         fprintf (stderr, "no device has been selected!\n");
-         print_help (argv [0]);
-         return 1;
-    } 
-
     /* Parse data/address */
     if (address_str == NULL) {
         fprintf (stderr, "no address has been selected!\n");
@@ -84,23 +68,22 @@ int main(int argc, char *argv[])
 
     uint64_t address = 0;
     if (sscanf (address_str, "%" PRIx64, &address) != 1) {
-        fprintf (stderr, "--address format is invalid!\n");
+        fprintf (stderr, "-a format is invalid!\n");
         print_help (argv [0]);
         return 1;
     }
 
-    /* Open device */
-    int err = pd_open (0, dev, devicefile_str);
-    if (err != 0) {
-         fprintf (stderr, "Could not open device %s, error = %d\n", devicefile_str, err);
-         return 1;
+    int device_number;
+    if (device_number_str == NULL || sscanf (device_number_str, "%d", &device_number) != 1) {
+        fprintf(stderr, "invalid or unspecified device number\n");
+        print_help(argv[0]);
+        return 1;
     }
 
-    struct pcie_bars bars = { .bar0 = pd_mapBAR (dev, 0), .bar4 = pd_mapBAR(dev, 4) };
-    if (bars.bar0 == NULL || bars.bar4 == NULL) {
-        fprintf (stderr, "Could not map BAR 0 and 4\n");
-        goto exit_unmap_bar;
-    }
+    pciDriver::PciDevice dev{device_number};
+    dev.open();
+
+    struct pcie_bars bars = { .bar0 = dev.mapBAR (0), .bar4 = dev.mapBAR(4) };
 
     if (verbose) {
         fprintf (stdout, "BAR 0 host address = %p\n", bars.bar0);
@@ -112,11 +95,6 @@ int main(int argc, char *argv[])
     bar4_read_u32s(&bars, address, &registers, ACQ_CORE_SIZE_32);
 
     decode_registers_print(&registers, stdout);
-
-exit_unmap_bar:
-    unmap_bar(dev, 0, bars.bar0);
-    unmap_bar(dev, 4, bars.bar4);
-    pd_close (dev);
 
     return 0;
 }
