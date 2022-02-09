240ec65cab823e7e7a4e803b2cceba52e93c4df8
Érico Nogueira <erico.rolim@lnls.br>
2022-02-09 11:27:37 -0300

    acq: show register in use when printing values.
    Activated with -v. This also required adding indentation to the output,
    to maintain readability, which allowed us to fix indentation for the
    channels more easily, and we added a custom_function mode to the printer
    to deal with more complicated registers and avoid re-implementing
    functionality in it.

--- decoders.cc
+++ decoders.cc
@@ -6,6 +6,7 @@
  */
 
 #include <cstring>
+#include <functional>
 #include <unordered_map>
 
 #include "decoders.h"
@@ -30,7 +31,11 @@ enum class PrinterType {
     value,
     value_hex,
     value_2c, /* 2's complement */
+    /* allow it to print its own value */
+    custom_function,
 };
+
+typedef std::function<void(FILE *, bool, uint32_t)> printing_function;
 
 class Printer {
     PrinterType type;
@@ -40,6 +45,8 @@ class Printer {
         const char *truth;
         const char *not_truth;
     } boolean_names;
+
+    printing_function custom_fn;
 
   public:
     Printer(const char *name, const char *description, PrinterType type):
@@ -61,6 +68,11 @@ class Printer {
             default:
                 break;
         }
+    }
+
+    Printer(const char *name, const char *description, PrinterType type, printing_function custom_fn):
+        type(type), name(name), description(description), custom_fn(custom_fn)
+    {
     }
 
     Printer(const char *name, const char *description, PrinterType type, const char *truth, const char *not_truth):
@@ -97,6 +109,9 @@ class Printer {
             case PrinterType::value_2c:
                 fprintf(f, "%s: %d\n", final_name, (int)(int32_t)value);
                 break;
+            case PrinterType::custom_function:
+                custom_fn(f, verbose, value);
+                break;
         }
     }
 };
@@ -106,7 +121,21 @@ void LnlsBpmAcqCore::print(FILE *f, bool verbose)
     #define I(name, ...) {name, {name, __VA_ARGS__}}
     static const std::unordered_map<const char *, Printer> printers({
         I("FSQ_ACQ_NOW", "Acquire data immediately and don't wait for any trigger", PrinterType::boolean, "acquire immediately", "wait on trigger"),
-        //I("FSM_STATE", "State machine status", PrinterType::value),
+        I("FSM_STATE", "State machine status", PrinterType::custom_function,
+            [](FILE *f, bool v, uint32_t value){
+                (void)v;
+                const char *fsm_states[] = {"IDLE", "PRE_TRIG", "WAIT_TRIG", "POST_TRIG", "DECR_SHOT"};
+                switch (value) {
+                    case 0: case 6: case 7:
+                        fprintf(f, "illegal (%u)", value);
+                        break;
+                    case 1: case 2: case 3: case 4: case 5:
+                        fprintf(f, "%s", fsm_states[value-1]);
+                        break;
+                }
+                fputc('\n', f);
+            }
+        ),
         I("FSM_ACQ_DONE", "FSM acquisition status", PrinterType::progress),
         I("FC_TRANS_DONE", "External flow control transfer status", PrinterType::boolean),
         I("FC_FULL", "External flow control FIFO full status", PrinterType::progress, "full (data may be lost)", "full"),
@@ -140,43 +169,39 @@ void LnlsBpmAcqCore::print(FILE *f, bool verbose)
     #undef I
 
     bool v = verbose;
-    unsigned t, t2;
+    unsigned indent = 0;
+    unsigned t;
 
-    auto print = [f, v](const char *name, uint32_t value) {
+    auto print_reg = [f, v, &indent](const char *reg, unsigned offset) {
+        if (v) {
+            fprintf(f, "%s (0x%02X)\n", reg, offset);
+            indent = 4;
+        }
+    };
+
+    auto print = [f, v, &indent](const char *name, uint32_t value) {
+        unsigned i = indent;
+        while (i--) fputc(' ', f);
         printers.at(name).print(f, v, value);
     };
 
     /* control register */
+    print_reg("control register", ACQ_CORE_CTL);
     t = regs.ctl;
     print("FSQ_ACQ_NOW", t & ACQ_CORE_CTL_FSM_ACQ_NOW);
 
     /* status register */
+    print_reg("status register", ACQ_CORE_STA);
     t = regs.sta;
 
-    fprintf(f, "FSM_STATE: ");
-    const char *fsm_states[] = {"IDLE", "PRE_TRIG", "WAIT_TRIG", "POST_TRIG", "DECR_SHOT"};
-    switch ((t2 = (t & ACQ_CORE_STA_FSM_STATE_MASK) >> ACQ_CORE_STA_FSM_STATE_SHIFT)) {
-        case 0:
-        case 6:
-        case 7:
-            fprintf(f, "illegal (%u)", t2);
-            break;
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            fprintf(f, "%s", fsm_states[t2-1]);
-            break;
-    }
-    fputc('\n', f);
-
+    print("FSM_STATE", (t & ACQ_CORE_STA_FSM_STATE_MASK) >> ACQ_CORE_STA_FSM_STATE_SHIFT);
     print("FSM_ACQ_DONE", t & ACQ_CORE_STA_FSM_ACQ_DONE);
     print("FC_TRANS_DONE", t & ACQ_CORE_STA_FC_TRANS_DONE);
     print("FC_FULL", t & ACQ_CORE_STA_FC_FULL);
     print("DDR3_TRANS_DONE", t & ACQ_CORE_STA_DDR3_TRANS_DONE);
 
     /* trigger configuration */
+    print_reg("trigger configuration", ACQ_CORE_TRIG_CFG);
     t = regs.trig_cfg;
     print("HW_TRIG_SEL", t & ACQ_CORE_TRIG_CFG_HW_TRIG_SEL);
     print("HW_TRIG_POL", t & ACQ_CORE_TRIG_CFG_HW_TRIG_POL);
@@ -185,32 +210,43 @@ void LnlsBpmAcqCore::print(FILE *f, bool verbose)
     print("INT_TRIG_SEL", ((t & ACQ_CORE_TRIG_CFG_INT_TRIG_SEL_MASK) >> ACQ_CORE_TRIG_CFG_INT_TRIG_SEL_SHIFT) + 1);
 
     /* trigger data config thresold */
+    print_reg("trigger data config threshold", ACQ_CORE_TRIG_DATA_CFG);
     t = regs.trig_data_cfg;
     print("THRES_FILT", (t & ACQ_CORE_TRIG_DATA_CFG_THRES_FILT_MASK) >> ACQ_CORE_TRIG_DATA_CFG_THRES_FILT_SHIFT);
 
     /* trigger */
+    print_reg("trigger data threshold", ACQ_CORE_TRIG_DATA_THRES);
     print("TRIG_DATA_THRES", regs.trig_data_thres);
+    print_reg("trigger delay", ACQ_CORE_TRIG_DLY);
     print("TRIG_DLY", regs.trig_dly);
 
     /* number of shots */
+    print_reg("number of shots", ACQ_CORE_SHOTS);
     t = regs.shots;
     print("NB", (t & ACQ_CORE_SHOTS_NB_MASK) >> ACQ_CORE_SHOTS_NB_SHIFT);
     print("MULTISHOT_RAM_SIZE_IMPL", t & ACQ_CORE_SHOTS_MULTISHOT_RAM_SIZE_IMPL);
     print("MULTISHOT_RAM_SIZE", (t & ACQ_CORE_SHOTS_MULTISHOT_RAM_SIZE_MASK) >> ACQ_CORE_SHOTS_MULTISHOT_RAM_SIZE_SHIFT);
 
     /* trigger address register */
+    print_reg("trigger address register", ACQ_CORE_TRIG_POS);
     print("TRIG_POS", regs.trig_pos);
     
     /* samples */
+    print_reg("pre-trigger samples", ACQ_CORE_PRE_SAMPLES);
     print("PRE_SAMPLES", regs.pre_samples);
+    print_reg("post-trigger samples", ACQ_CORE_POST_SAMPLES);
     print("POST_SAMPLES", regs.post_samples);
+    print_reg("sample counter", ACQ_CORE_SAMPLES_CNT);
     print("SAMPLES_CNT", regs.samples_cnt);
 
     /* ddr3 addresses */
+    print_reg("DDR3 Start Address", ACQ_CORE_DDR3_START_ADDR);
     print("DDR3_START_ADDR", regs.ddr3_start_addr);
+    print_reg("DDR3 End Address", ACQ_CORE_DDR3_END_ADDR);
     print("DDR3_END_ADDR", regs.ddr3_end_addr);
 
     /* acquisition channel control */
+    print_reg("acquisition channel control", ACQ_CORE_ACQ_CHAN_CTL);
     t = regs.acq_chan_ctl;
     /* will be used to determine how many channels to show in the next block */
     unsigned num_chan = (t & ACQ_CORE_ACQ_CHAN_CTL_NUM_CHAN_MASK) >> ACQ_CORE_ACQ_CHAN_CTL_NUM_CHAN_SHIFT;
@@ -230,7 +266,9 @@ void LnlsBpmAcqCore::print(FILE *f, bool verbose)
     memcpy(p, &regs.ch0_desc, sizeof p);
     for (unsigned i = 0; i < num_chan; i++) {
         uint32_t desc = p[i*2], adesc = p[i*2 + 1];
-        fprintf(f, "    channel %u:\n", i);
+        fprintf(f, "channel %u description and atom description (%02X and %02X):\n",
+            i, (unsigned)ACQ_CORE_CH0_DESC + 4*i, (unsigned)ACQ_CORE_CH0_ATOM_DESC + 4*i);
+        indent = 4;
         print("INT_WIDTH", (desc & ACQ_CORE_CH0_DESC_INT_WIDTH_MASK) >> ACQ_CORE_CH0_DESC_INT_WIDTH_SHIFT);
         print("NUM_COALESCE", (desc & ACQ_CORE_CH0_DESC_NUM_COALESCE_MASK) >> ACQ_CORE_CH0_DESC_NUM_COALESCE_SHIFT);
         print("NUM_ATOMS", (adesc & ACQ_CORE_CH0_ATOM_DESC_NUM_ATOMS_MASK) >> ACQ_CORE_CH0_ATOM_DESC_NUM_ATOMS_SHIFT);
