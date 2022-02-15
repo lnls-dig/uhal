f8f21e14fdc514df49621ac6f0b7d6de35d3c974
Érico Nogueira <erico.rolim@lnls.br>
2022-02-15 10:55:24 -0300

    Use ACQ controller class.
    - Add CLI parameters for each parameter of interest (fix indentation
      on the way as well)
    - Be explicit about the type of args to avoid confusion. Also make it
      const (disallows using [] to access members, needs .at())
    - We are using docopt in the correct manner: check for the flag as a
      boolean, and only then capture the value passed to it

--- decode-reg.cc
+++ decode-reg.cc
@@ -25,21 +25,32 @@ R"(Simple FPGA Register Reader
 
 Usage:
     decode-reg [-hv] -b <device> -a <base_address>
+    decode-reg [-hv] -r -b <device> -a <base_address> -c <channel> -n <pre_samples> -p <post_samples> -s <shots> [-t <trigger>] [-e <data_trigger_threshold>] [-l] [-z <data_trigger_selection>] [-i <data_trigger_filter>] [-C <data_trigger_channel>] [-d <trigger_delay>]
 
--h                 Display this usage information
--v                 Verbose output
--b <device>        Device number as used in /dev/fpga-N
--a <base_address>  Base address to start reads from (in hex)
+  -h                 Display this usage information
+  -v                 Verbose output
+  -r                 Perform read acquisition
+  -l                 Use negative edge data trigger
+  -b <device>        Device number as used in /dev/fpga-N
+  -a <base_address>  Base address to start reads from (in hex)
+  -c <channel>       Channel to be used for acquisition
 )";
+
+
+static void try_long(unsigned &dest, const docopt::Options &args, const char *flag, const char *parameter)
+{
+    if (args.at(flag))
+        dest = args.at(parameter).asLong();
+}
 
 int main(int argc, char *argv[])
 {
-    auto args = docopt::docopt(usage, {argv+1, argv+argc}, true);
+    const docopt::Options args = docopt::docopt(usage, {argv+1, argv+argc}, true);
 
-    bool verbose = args["-v"].asBool();
-    std::string address_str = args["<base_address>"].asString();
+    bool verbose = args.at("-v").asBool();
+    std::string address_str = args.at("<base_address>").asString();
     size_t address = std::stol(address_str, nullptr, 16);
-    int device_number = args["<device>"].asLong();
+    int device_number = args.at("<device>").asLong();
 
     pciDriver::PciDevice dev{device_number};
     dev.open();
@@ -49,6 +60,24 @@ int main(int argc, char *argv[])
     if (verbose) {
         fprintf (stdout, "BAR 0 host address = %p\n", bars.bar0);
         fprintf (stdout, "BAR 4 host address = %p\n", bars.bar4);
+    }
+
+    if (args.at("-r").asBool()) {
+        LnlsBpmAcqCoreController ctl{&bars, address};
+        ctl.channel = args.at("<channel>").asLong();
+        ctl.pre_samples = args.at("<pre_samples>").asLong();
+        ctl.post_samples = args.at("<post_samples>").asLong();
+        ctl.number_shots = args.at("<shots>").asLong();
+        if (args.at("-t")) ctl.trigger_type = args.at("<trigger>").asString();
+        try_long(ctl.data_trigger_threshold, args, "-e", "<data_trigger_threshold>");
+        ctl.data_trigger_polarity_p = !args.at("-l");
+        try_long(ctl.data_trigger_sel, args, "-z", "<data_trigger_selection>");
+        try_long(ctl.data_trigger_filt, args, "-i", "<data_trigger_filter>");
+        try_long(ctl.data_trigger_channel, args, "-C", "<data_trigger_channel>");
+        try_long(ctl.trigger_delay, args, "-d", "<trigger_delay>");
+
+        ctl.write_config();
+        ctl.start_acquisition();
     }
 
     std::unique_ptr<RegisterDecoder> dec{new LnlsBpmAcqCore};
