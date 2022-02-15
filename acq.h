923ffc008f1dacd3ae92c192a6666ceefb6a3488
Érico Nogueira <erico.rolim@lnls.br>
2022-02-15 10:46:56 -0300

    acq: implement a controller.
    This controller allows for easy parameter configuration, and can
    subsequently write those parameters to the hardware.
    
    Since we are here, fix the include in decode-reg.cc to avoid using an
    internal header.

---
+++ acq.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022 CNPEM (cnpem.br)
+ * Author: Érico Nogueira <erico.rolim@lnls.br>
+ *
+ * Released according to the GNU GPL, version 3 or any later version.
+ */
+
+#ifndef ACQ_H
+#define ACQ_H
+
+#include <string>
+
+#include "decoders.h"
+
+class LnlsBpmAcqCoreController {
+    const struct pcie_bars *bars;
+    size_t addr;
+
+    struct acq_core regs{};
+
+  public:
+    LnlsBpmAcqCoreController(const struct pcie_bars *bars, size_t addr):
+        bars(bars), addr(addr)
+    {
+    }
+
+    unsigned channel = 0;
+    unsigned pre_samples = 4;
+    unsigned post_samples = 16;
+    unsigned number_shots = 1;
+    std::string trigger_type = "immediate";
+    unsigned data_trigger_threshold = 0;
+    bool data_trigger_polarity_p = true;
+    unsigned data_trigger_sel = 0;
+    unsigned data_trigger_filt = 1;
+    unsigned data_trigger_channel = 0;
+    unsigned trigger_delay = 0;
+
+    void encode_config();
+    void write_config();
+    void start_acquisition();
+};
+
+#endif
