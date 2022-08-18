9f2acd11e3336d729f6aeb187283658ba2ae5953
Érico Nogueira <erico.rolim@lnls.br>
2022-08-18 11:43:19 -0300

    pcie: clean up pcie.h and pcie.c.
    - move implementation macros and definitions into pcie.c, they don't
      need to be public
    - use bar0_write instead of SET_PG macro
    - remove SET_PG macro
    - move the definition of the status register and the wishbone reset flag
      to their own block

--- util/pcie.c
+++ util/pcie.c
@@ -19,6 +19,69 @@
 
 #include "pcie.h"
 
+/* PCIe Page constants */
+/* SDRAM is accesses via 32-bit BAR (32-bit addressing) */
+#define PCIE_SDRAM_PG_SHIFT                 0           /* bits */
+#define PCIE_SDRAM_PG_MAX                   20          /* bits */
+#define PCIE_SDRAM_PG_SIZE                  (1<<PCIE_SDRAM_PG_MAX)  /* in Bytes (8-bit) */
+#define PCIE_SDRAM_PG_MASK                  ((PCIE_SDRAM_PG_SIZE-1) << \
+                                                PCIE_SDRAM_PG_SHIFT)
+
+/* Wishbone is accessed via 64-bit BAR (64-bit addressed) */
+#define PCIE_WB_PG_SHIFT                    0           /* bits */
+#define PCIE_WB_PG_MAX                      16          /* bits */
+#define PCIE_WB_PG_SIZE                     (1<<PCIE_WB_PG_MAX)  /* in Bytes (8-bit) */
+#define PCIE_WB_PG_MASK                     ((PCIE_WB_PG_SIZE-1) << \
+                                                PCIE_WB_PG_SHIFT)
+
+/* PCIe SDRAM Address Page number and offset extractor */
+#define PCIE_ADDR_SDRAM_PG_OFFS(addr)       ((addr & PCIE_SDRAM_PG_MASK) >> \
+                                                PCIE_SDRAM_PG_SHIFT)
+#define PCIE_ADDR_SDRAM_PG(addr)            ((addr & ~PCIE_SDRAM_PG_MASK) >> \
+                                                PCIE_SDRAM_PG_MAX)
+
+/* PCIe WB Address Page number and offset extractor */
+#define PCIE_ADDR_WB_PG_OFFS(addr)          ((addr & PCIE_WB_PG_MASK) >> \
+                                                PCIE_WB_PG_SHIFT)
+#define PCIE_ADDR_WB_PG(addr)               ((addr & ~PCIE_WB_PG_MASK) >> \
+                                                PCIE_WB_PG_MAX)
+
+#define WB_QWORD_ACC                        3           /* 64-bit addressing */
+#define WB_DWORD_ACC                        2           /* 32-bit addressing */
+#define WB_WORD_ACC                         1           /* 16-bit addressing */
+#define WB_BYTE_ACC                         0           /* 8-bit addressing */
+
+/* FPGA PCIe registers. These are inside bar0 and must match
+ * the FPGA firmware */
+#define PCIE_CFG_REG_SDRAM_PG               (7 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_WB_PG                  (9 << WB_DWORD_ACC)
+
+/* First register of the block*/
+#define PCIE_CFG_REG_DMA_US_BASE            (11 << WB_DWORD_ACC)
+
+/* Offset for DMA registers */
+#define PCIE_CFG_REG_DMA_PAH                (0 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_PAL                (1 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_HAH                (2 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_HAL                (3 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_BDAH               (4 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_BDAL               (5 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_LENG               (6 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_CTRL               (7 << WB_DWORD_ACC)
+#define PCIE_CFG_REG_DMA_STA                (8 << WB_DWORD_ACC)
+
+/* Relevant values for DMA registers */
+#define PCIE_CFG_DMA_CTRL_AINC (1 << 15)
+#define PCIE_CFG_DMA_CTRL_BAR_SHIFT 16
+#define PCIE_CFG_DMA_CTRL_LAST (1 << 24)
+#define PCIE_CFG_DMA_CTRL_VALID (1 << 25)
+#define PCIE_CFG_DMA_STA_DONE (1 << 0)
+#define PCIE_CFG_DMA_STA_TIMEOUT (1 << 4)
+
+/* Other registers and values */
+#define PCIE_CFG_REG_EB_STACON              (36 << WB_DWORD_ACC)
+#define PCIE_CFG_TX_CTRL_CHANNEL_RST 0x0A
+
 static void bar_generic_read_v(const struct pcie_bars *bars, size_t addr, void *dest, size_t n,
     uint32_t (*fn)(const struct pcie_bars *bars, size_t addr))
 {
@@ -49,14 +112,12 @@ static void bar0_write(const struct pcie_bars *bars, size_t addr, uint32_t value
 
 static void set_sdram_pg(const struct pcie_bars *bars, int num)
 {
-    volatile uint32_t *bar0 = bars->bar0;
-    SET_PG(bar0, PCIE_CFG_REG_SDRAM_PG, num);
+    bar0_write(bars, PCIE_CFG_REG_SDRAM_PG, num);
 }
 
 static void set_wb_pg(const struct pcie_bars *bars, int num)
 {
-    volatile uint32_t *bar0 = bars->bar0;
-    SET_PG(bar0, PCIE_CFG_REG_WB_PG, num);
+    bar0_write(bars, PCIE_CFG_REG_WB_PG, num);
 }
 
 static inline volatile uint32_t *bar2_get_u32p_small(const struct pcie_bars *bars, size_t addr, unsigned index)
