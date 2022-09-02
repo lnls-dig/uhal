5475dfe25c7a9b5ca3c885c4d0583fb645f4b2cf
Érico Nogueira <erico.rolim@lnls.br>
2022-09-02 09:44:41 -0300

    decoders: extend RegisterDecoder to also hold data.
    Decoders were only used to print decoded to a FILE stream, which made
    them less useful as classes that can be used to build more complex
    applications (such as an IOC).
    
    Since we already have code that prints strings and values decoded from
    hardware, and this code can be easily converted to do something else, we
    are using strings as the keys. This gives us an additional advantage
    when developing the IOC using the asyn module, since it will be able to
    simply loop through its parameters and use their names as keys, without
    adding a new line of code for each parameter, to be read from the
    decoder.
    
    Since printing now happens in a function in an upper layer, we need a
    printers member. This also makes it necessary to install the printer.h
    header.
    
    The conversion is in progress, so compatibility members are kept around
    (namely, constructor without printers parameter and dummy decode()
    impl), but these should be removed as soon as possible.

--- util/decoders.cc
+++ util/decoders.cc
@@ -7,6 +7,11 @@
 
 #include "decoders.h"
 
+RegisterDecoder::RegisterDecoder(std::unordered_map<const char *, Printer> printers):
+    printers(printers)
+{
+}
+
 void RegisterDecoder::set_devinfo(const struct sdb_device_info &new_devinfo)
 {
     devinfo = new_devinfo;
@@ -15,4 +20,52 @@ void RegisterDecoder::set_devinfo(const struct sdb_device_info &new_devinfo)
 void RegisterDecoder::read(struct pcie_bars *bars)
 {
     bar4_read_v(bars, devinfo.start_addr, read_dest, read_size);
+}
+
+void RegisterDecoder::print(FILE *f, bool verbose)
+{
+    decode();
+
+    unsigned indent = 0;
+
+    auto print = [this, f, verbose, &indent](const char *name, auto value) {
+        printers.at(name).print(f, verbose, indent, value);
+    };
+
+    for (const auto name: general_data_order) {
+        print(name, general_data.at(name));
+    }
+
+    if (number_of_channels) for (unsigned i = 0; i < *number_of_channels; i++) {
+        if (channel && *channel != i) {
+            i++;
+            continue;
+        }
+        fprintf(f, "channel %u:\n", i++);
+        indent = 4;
+
+        for (const auto name: channel_data_order) {
+            print(name, channel_data.at(name).at(i));
+        }
+    }
+}
+
+int32_t RegisterDecoder::get_general_data(const char *name)
+{
+    try {
+        return general_data.at(name);
+    } catch (std::out_of_range &e) {
+        fprintf(stderr, "%s: bad key '%s'\n", __func__, name);
+        throw e;
+    }
+}
+
+int32_t RegisterDecoder::get_channel_data(const char *name, unsigned channel_index)
+{
+    try {
+        return channel_data.at(name).at(channel_index);
+    } catch (std::out_of_range &e) {
+        fprintf(stderr, "%s: bad key '%s'\n", __func__, name);
+        throw e;
+    }
 }
