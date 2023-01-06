1dfe48eaa7bf99e6c3719887bf40bf468532cbc8
Érico Nogueira <erico.rolim@lnls.br>
2023-01-06 15:12:41 -0300

    decode-reg: add trigger_iface as a decoder.

--- app/decode-reg.cc
+++ app/decode-reg.cc
@@ -25,6 +25,7 @@
 #include "fofb_cc.h"
 #include "fofb_processing.h"
 #include "lamp.h"
+#include "trigger_iface.h"
 #include "trigger_mux.h"
 
 using namespace std::chrono_literals;
@@ -148,6 +149,8 @@ int main(int argc, char *argv[])
             dec = std::make_unique<fofb_cc::Core>(bars);
         } else if (type == "fofb_processing") {
             dec = std::make_unique<fofb_processing::Core>(bars);
+        } else if (type == "trigger_iface") {
+            dec = std::make_unique<trigger_iface::Core>(bars);
         } else if (type == "trigger_mux") {
             dec = std::make_unique<trigger_mux::Core>(bars);
         } else {
