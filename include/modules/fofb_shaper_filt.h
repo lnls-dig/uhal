6847e60e0f5b969153ed5b4927bf58065544aac5
Érico Nogueira <erico.rolim@lnls.br>
2023-09-22 15:37:29 -0300

    fofb_shaper_filt: add module.
    The static assertion for the amount of coefficients was written to make
    sure the sizeof expressions were correct, but it also works as a check
    for if the ABI ever changes.
    
    The discrepancy in the amount of coefficients for Core and Controller is
    intentional: for Core, we have only the actually relevant coefficients;
    for Controller, we support the highest amount, so library clients don't
    need to implement logic based on num_biquads. However, we can loop only
    through the valid coefficients in both cases, for a slight optimization.
    
    When actually writing to hardware, only the valid coefficients can be
    written: both the 3 unused coefficients and the unused biquads must be
    skipped. This was a bug noticed during development of the library, and
    might be fixed in the future [1].
    
    [1] https://github.com/lnls-dig/fofb-ctrl-gw/pull/36

---
+++ include/modules/fofb_shaper_filt.h
@@ -0,0 +1,59 @@
+#ifndef FOFB_SHAPER_FILT_H
+#define FOFB_SHAPER_FILT_H
+
+#include <vector>
+#include <memory>
+
+#include "controllers.h"
+#include "decoders.h"
+
+namespace fofb_shaper_filt {
+
+struct filter_coefficients {
+    filter_coefficients();
+    void set_num_biquads(size_t);
+    std::vector<std::vector<double>> values;
+};
+
+/* forward declaration */
+struct wb_fofb_shaper_filt_regs;
+
+class Core: public RegisterDecoder {
+    std::unique_ptr<struct wb_fofb_shaper_filt_regs> regs_storage;
+    struct wb_fofb_shaper_filt_regs &regs;
+
+    void decode() override;
+    void read_monitors() override;
+    void decode_monitors() override;
+
+  public:
+    Core(struct pcie_bars &);
+    ~Core();
+
+    void print(FILE *, bool) const override;
+
+    filter_coefficients coefficients;
+};
+
+class Controller: public RegisterController {
+  protected:
+    std::unique_ptr<struct wb_fofb_shaper_filt_regs> regs_storage;
+    struct wb_fofb_shaper_filt_regs &regs;
+
+    unsigned fixed_point_coeff, num_biquads;
+
+    void set_devinfo_callback() override;
+    void encode_params() override;
+
+  public:
+    Controller(struct pcie_bars &);
+    ~Controller();
+
+    void write_params() override;
+
+    filter_coefficients coefficients;
+};
+
+} /* namespace fofb_shaper_filt */
+
+#endif
---
+++ modules/fofb_shaper_filt.cc
@@ -0,0 +1,153 @@
+#include <algorithm>
+#include <stdexcept>
+
+#include "pcie.h"
+#include "printer.h"
+#include "util.h"
+#include "modules/fofb_shaper_filt.h"
+
+namespace fofb_shaper_filt {
+
+#include "hw/wb_fofb_shaper_filt_regs.h"
+
+static_assert(WB_FOFB_SHAPER_FILT_REGS_CH_SIZE + WB_FOFB_SHAPER_FILT_REGS_CH == offsetof(wb_fofb_shaper_filt_regs, ch[1]));
+static_assert(WB_FOFB_SHAPER_FILT_REGS_NUM_BIQUADS == offsetof(wb_fofb_shaper_filt_regs, num_biquads));
+
+namespace {
+    const size_t
+        NUM_CHANNELS = 12,
+        NUM_COEFFS =  sizeof(wb_fofb_shaper_filt_regs::ch::coeffs) / sizeof(wb_fofb_shaper_filt_regs::ch::coeffs::val),
+        NUM_BIQUADS = 10,
+        COEFFS_PER_BIQUAD = 5,
+        UNUSED_PER_BIQUAD = 3,
+        TOTAL_PER_BIQUAD = COEFFS_PER_BIQUAD + UNUSED_PER_BIQUAD;
+    static_assert(NUM_COEFFS == NUM_BIQUADS * TOTAL_PER_BIQUAD);
+
+    constexpr unsigned FOFB_SHAPER_FILT_DEVID = 0xf65559b2;
+    struct sdb_device_info ref_devinfo = {
+        .vendor_id = LNLS_VENDORID,
+        .device_id = FOFB_SHAPER_FILT_DEVID,
+        .abi_ver_major = 1
+    };
+}
+
+filter_coefficients::filter_coefficients():
+  values(NUM_CHANNELS)
+{
+}
+
+void filter_coefficients::set_num_biquads(size_t num_biquads)
+{
+    for (auto &c: values) c.resize(num_biquads * COEFFS_PER_BIQUAD);
+}
+
+Core::Core(struct pcie_bars &bars):
+    RegisterDecoder(bars, ref_devinfo, { }),
+    CONSTRUCTOR_REGS(struct wb_fofb_shaper_filt_regs)
+{
+    set_read_dest(regs);
+    number_of_channels = NUM_CHANNELS;
+}
+Core::~Core() = default;
+
+/* no monitored values */
+void Core::read_monitors()
+{
+}
+void Core::decode_monitors()
+{
+}
+
+void Core::decode()
+{
+    uint32_t t;
+    add_general("NUM_BIQUADS", regs.num_biquads);
+    unsigned num_biquads = regs.num_biquads;
+
+    t = regs.coeffs_fp_repr;
+    uint32_t int_width = extract_value<uint8_t>(t, WB_FOFB_SHAPER_FILT_REGS_COEFFS_FP_REPR_INT_WIDTH_MASK);
+    add_general("INT_WIDTH", int_width);
+    add_general("FRAC_WIDTH", extract_value<uint8_t>(t, WB_FOFB_SHAPER_FILT_REGS_COEFFS_FP_REPR_FRAC_WIDTH_MASK));
+
+    uint32_t fixed_point_coeff = 32 - int_width;
+
+    coefficients.set_num_biquads(num_biquads);
+
+    for (unsigned i = 0; i < NUM_CHANNELS; i++) {
+        for (unsigned j = 0; j < num_biquads; j++) {
+            for (unsigned k = 0; k < COEFFS_PER_BIQUAD; k++) {
+                coefficients.values[i][k + COEFFS_PER_BIQUAD * j] =
+                    fixed2float(regs.ch[i].coeffs[k + TOTAL_PER_BIQUAD * j].val, fixed_point_coeff);
+            }
+        }
+    }
+}
+
+void Core::print(FILE *f, bool verbose) const
+{
+    RegisterDecoder::print(f, verbose);
+
+    if (channel) {
+        fprintf(f, "channel %u filter coefficients: ", *channel);
+        for (auto c: coefficients.values[*channel]) {
+            fprintf(f, "%lf, ", c);
+        }
+        fputs("\n", f);
+
+        if (verbose) {
+            fprintf(f, "channel %u raw filter coefficients: ", *channel);
+            for (auto c: regs.ch[*channel].coeffs) {
+                fprintf(f, "%#08x, ", (unsigned)c.val);
+            }
+            fputs("\n", f);
+        }
+    }
+}
+
+Controller::Controller(struct pcie_bars &bars):
+    RegisterController(bars, ref_devinfo),
+    CONSTRUCTOR_REGS(struct wb_fofb_shaper_filt_regs)
+{
+    set_read_dest(regs);
+
+    coefficients.set_num_biquads(NUM_BIQUADS);
+}
+Controller::~Controller() = default;
+
+void Controller::set_devinfo_callback()
+{
+    regs.coeffs_fp_repr = bar4_read(&bars, addr + WB_FOFB_SHAPER_FILT_REGS_COEFFS_FP_REPR);
+    fixed_point_coeff = 32 - extract_value<uint8_t>(regs.coeffs_fp_repr, WB_FOFB_SHAPER_FILT_REGS_COEFFS_FP_REPR_INT_WIDTH_MASK);
+
+    regs.num_biquads = bar4_read(&bars, addr + WB_FOFB_SHAPER_FILT_REGS_NUM_BIQUADS);
+    num_biquads = regs.num_biquads;
+}
+
+void Controller::encode_params()
+{
+    for (unsigned i = 0; i < NUM_CHANNELS; i++) {
+        for (unsigned j = 0; j < num_biquads; j++) {
+            for (unsigned k = 0; k < COEFFS_PER_BIQUAD; k++) {
+                regs.ch[i].coeffs[k + TOTAL_PER_BIQUAD * j].val =
+                    float2fixed(coefficients.values[i][k + COEFFS_PER_BIQUAD * j], fixed_point_coeff);
+            }
+        }
+    }
+}
+
+void Controller::write_params()
+{
+    encode_params();
+
+    for (unsigned i = 0; i < NUM_CHANNELS; i++) {
+        for (unsigned j = 0; j < num_biquads; j++) {
+            bar4_write_v(
+                &bars,
+                addr + WB_FOFB_SHAPER_FILT_REGS_CH_COEFFS + i * sizeof(regs.ch[0]) + TOTAL_PER_BIQUAD * j * sizeof(uint32_t),
+                &regs.ch[i].coeffs[TOTAL_PER_BIQUAD * j].val,
+                COEFFS_PER_BIQUAD * sizeof(uint32_t));
+        }
+    }
+}
+
+} /* namespace fofb_shaper_filt */
