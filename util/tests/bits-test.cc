79e967d5eca76bd6c77bf1c98438badb8b87e760
Érico Nogueira <erico.rolim@lnls.br>
2024-01-08 11:43:12 -0300

    util: add extract_value function tests.

--- util/tests/bits-test.cc
+++ util/tests/bits-test.cc
@@ -15,3 +15,29 @@ TEST_CASE("template extract_value signed", "[bits-test]")
     CHECK(extract_value<int16_t>(0x7fe00, 0xffff0) == 0x7fe0);
     CHECK(extract_value<int16_t>(0xffe00, 0xffff0) == -32);
 }
+
+TEST_CASE("extract_value unsigned", "[bits-test]")
+{
+    CHECK(extract_value(0xfe00, 0xff00, false) == 254);
+    CHECK(extract_value(0xffe00, 0xffff0, false) == 0xffe0);
+}
+
+TEST_CASE("extract_value signed", "[bits-test]")
+{
+    CHECK(extract_value(0xfe00, 0xff00, true) == -2);
+    CHECK(extract_value(0xffe00, 0xffff0, true) == -32);
+}
+
+TEST_CASE("extract_value signed unhandled width", "[bits-test]")
+{
+    CHECK_THROWS_AS(extract_value(0, 0x7f00, true), std::logic_error);
+}
+
+TEST_CASE("extract_value all bits set", "[bits-test]")
+{
+    CHECK(extract_value(0xffff, 0xffff, false) == 0xffff);
+    CHECK(extract_value(0xffff, 0xffff, true) == -1);
+
+    CHECK((uint32_t)extract_value(0xffffffff, 0xffffffff, false) == 0xffffffffU);
+    CHECK(extract_value(0xffffffff, 0xffffffff, true) == -1);
+}
